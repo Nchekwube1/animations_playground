63415e3e02a4630b422c3282bbb496daaf221b09
Unekwe Francis <unekwenchekwube@gmail.com>
2022-07-18 18:54:43 +0100

    added wobble animation

--- src/screens/Test.tsx
+++ src/screens/Test.tsx
@@ -1,18 +1,35 @@
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
 import React from 'react';
-import globalStyles, {height, width} from '../styles/globalstyles';
+import globalStyles, {height} from '../styles/globalstyles';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
+  withRepeat,
+  withSequence,
   withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 const BOX_WIDTH = 70;
+const ANGLE = 30;
 
 const Test = () => {
   const offset = useSharedValue(0);
+  const rotation = useSharedValue(0);
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{translateX: withSpring(offset.value * 255)}],
+      transform: [
+        {
+          translateX: withSpring(offset.value * 255, {
+            damping: 10,
+            stiffness: 71,
+          }),
+        },
+      ],
+    };
+  });
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{rotateZ: withSpring(`${rotation.value}deg`)}],
     };
   });
   return (
@@ -26,6 +43,20 @@ const Test = () => {
       ]}>
       <Animated.View style={[styles.box, animatedStyles]} />
       <Button onPress={() => (offset.value = Math.random())} title="Move" />
+
+      <Animated.View style={[styles.box, animatedStyle]} />
+      <Button
+        title="wobble"
+        onPress={() => {
+          // rotation.value = Math.random() * 180
+          rotation.value = withSequence(
+            withTiming(-ANGLE, {duration: 50}),
+            withRepeat(withTiming(ANGLE, {duration: 100}), 6, true),
+            withTiming(0, {duration: 50}),
+          );
+          // withRepeat(withTiming(10), 6, true);
+        }}
+      />
     </View>
   );
 };
