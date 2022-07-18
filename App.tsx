40136bf024df742340a0005c4eebca09aafa1578
Unekwe Francis <unekwenchekwube@gmail.com>
2022-07-18 17:57:22 +0100

    adding common styles

--- App.tsx
+++ App.tsx
@@ -9,14 +9,16 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
 const STATUSBAR_HEIGHT = getStatusBarHeight();
 const App = () => {
   return (
-    <SafeAreaView style={{height: STATUSBAR_HEIGHT, backgroundColor: 'red'}}>
-      <StatusBar barStyle={'light-content'} />
-    </SafeAreaView>
+    <View style={{...StyleSheet.absoluteFillObject}}>
+      <SafeAreaView style={{height: STATUSBAR_HEIGHT, backgroundColor: 'red'}}>
+        <StatusBar barStyle={'light-content'} />
+      </SafeAreaView>
+    </View>
   );
 };
 
---
+++ src/styles/globalstyles.ts
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  px10: {
+    paddingHorizontal: 10,
+  },
+  px20: {
+    paddingHorizontal: 20,
+  },
+  px30: {
+    paddingHorizontal: 30,
+  },
+  px40: {
+    paddingHorizontal: 40,
+  },
+  px50: {
+    paddingHorizontal: 50,
+  },
+  py10: {
+    paddingHorizontal: 10,
+  },
+  py20: {
+    paddingHorizontal: 20,
+  },
+  py30: {
+    paddingHorizontal: 30,
+  },
+  py40: {
+    paddingHorizontal: 40,
+  },
+  py50: {
+    paddingHorizontal: 50,
+  },
+});
+
+export default globalStyles;
