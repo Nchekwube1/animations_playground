1a7b569bebb3b2acb1d107ee6ea4da3eeb37a7f3
Unekwe Francis <unekwenchekwube@gmail.com>
2022-07-22 23:44:49 +0100

    added data file contents

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -2,9 +2,53 @@ interface data {
   imageUrl: string;
   title: string;
   desc: string;
+  price: string;
 }
 
-export const data: data[] = [{}];
+export const data: data[] = [
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2xvdGhlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'T-shirts',
+    desc: 'Very comfortable and affordable tees',
+    price: '$450',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1516762689617-e1cffcef479d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    title: 'Winter outfit',
+    desc: 'Look classy and stylish this winter',
+    price: '$247',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1434389677669-e08b4cac3105?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2xvdGhlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    title: 'Crop top',
+    desc: 'Balenciaga crop top',
+    price: '$735',
+  },
+  {
+    imageUrl:
+      'https://media.istockphoto.com/photos/funny-brown-cat-in-a-knitted-hat-closeup-copy-space-picture-id1363634487?b=1&k=20&m=1363634487&s=170667a&w=0&h=Y2nMpGpYdf42JXiBspg4QHJTX07AvFs8UzVmTKUVJps=',
+    title: 'Beenie',
+    desc: 'Silky soft beenie',
+    price: '$130',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1604176354204-9268737828e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    title: 'Jeans',
+    desc: 'Highest quality denim jeans',
+    price: '$750',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1631041556964-57f2686f1113?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+    title: 'Towel',
+    desc: 'Silky soft towels for men',
+    price: '$346',
+  },
+];
 export const logoUrls = [
   'https://images.unsplash.com/photo-1572584642822-6f8de0243c93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNwbGFzaCUyMHNhbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
   'https://images.unsplash.com/photo-1576748872293-f4972ceda096?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNwbGFzaCUyMHNhbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
